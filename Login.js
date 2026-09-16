4cdd435a22ac897a5fff5de5541734afc9b03289
Leezuo1 <gsahai1234@gmail.com>
2026-09-16 13:38:55 +0700

    Create Login.js

---
+++ Login.js
@@ -0,0 +1,15 @@
+// login.js
+// Logic kiểm tra đăng nhập đơn giản
+
+function checkLogin(user, pass) {
+  if (user === 'admin' && pass === '123') {
+    return true;
+  }
+  return false;
+}
+
+// Hỗ trợ cả 2 môi trường: trình duyệt (dùng trực tiếp trong index.html)
+// và Node.js / Jest (dùng require để test)
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { checkLogin };
+}
