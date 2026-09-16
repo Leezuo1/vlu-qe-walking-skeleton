e81159af535349374a1ef6d26b2c0359ccac43f0
Leezuo1 <gsahai1234@gmail.com>
2026-09-16 13:53:43 +0700

    phá xong h sửa lại :V

--- login.js
+++ login.js
@@ -2,7 +2,7 @@
 // Logic kiểm tra đăng nhập đơn giản
 
 function checkLogin(user, pass) {
-  if (user === 'admin' && pass === '1234') {
+  if (user === 'admin' && pass === '123') {
     return true;
   }
   return false;
